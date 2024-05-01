5f66b9a580d66eeadd203c985022c640bd64ee14
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-05-01 15:07:50 +0200

    Init project with needed interface

---
+++ src/EscampeBoard.java
@@ -0,0 +1,36 @@
+public class EscampeBoard implements Partie1
+{
+    @Override
+    public void setFromFile(String fileName) {
+
+    }
+
+    @Override
+    public void saveToFile(String fileName) {
+
+    }
+
+    @Override
+    public boolean isValidMove(String move, String player) {
+        return false;
+    }
+
+    @Override
+    public String[] possiblesMoves(String player) {
+        return new String[0];
+    }
+
+    @Override
+    public void play(String move, String player) {
+
+    }
+
+    @Override
+    public boolean gameOver() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start an Escampe Game :) ?");
+    }
+}
